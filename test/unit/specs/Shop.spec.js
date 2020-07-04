b852f510a2fb8f1bfe778dd3e1e818d6c9b3fdf3
kevin Martínez <kevinccbsg@gmail.com>
2020-07-04 18:01:46 +0200

    fix: eslint errors

--- test/unit/specs/Shop.spec.js
+++ test/unit/specs/Shop.spec.js
@@ -1,17 +1,17 @@
 import Vue from 'vue'
 import Shop from '@/components/Shop'
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils'
 
 describe('Shop.vue', () => {
   it('should render correct contents', () => {
-    const msg = 'This is an ACME page';
+    const msg = 'This is an ACME page'
     const wrapper = shallowMount(Shop, {
-      propsData: { msg },
-    });
-    expect(wrapper.text()).toMatch(msg);
-    //. const Constructor = Vue.extend(Shop)
-    //. const vm = new Constructor().$mount()
-    //. expect(vm.$el.querySelector('.hello h1').textContent)
-    //.   .toEqual('Welcome to Your Vue.js App')
+      propsData: { msg }
+    })
+    expect(wrapper.text()).toMatch(msg)
+    // . const Constructor = Vue.extend(Shop)
+    // . const vm = new Constructor().$mount()
+    // . expect(vm.$el.querySelector('.hello h1').textContent)
+    // .   .toEqual('Welcome to Your Vue.js App')
   })
 })
